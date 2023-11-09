507cfbf6ba8cc5291c765ca4efb118c2c13045f0
Prateek Angel <prateekangel8127@gmail.com>
2023-11-09 00:21:15 -0500

    git commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<?php get_footer(); ?>
+<div class="footer">
+        <h2>Get More Stuff</h2>
+        <p> Subscribe to our mailing list and get interesting stuff and updates to
+            your email box.
+        </p>
+        <label for="e-mail" class="content-mail">Email Address</label><br>
+        <input type="text" class="content-net">
+    </div>
+    <div class="grid">
+        <div class="foot-image">
+            <img src="logo.webp">
+            <p>ChurchBuzz is your space to write and <br>
+                share stories on things that matter,<br>
+                and create an impact. Email us at<a href="#"> <br>
+                    churchbuzz.in@gmail.com </a> get <br>in touch with us.
+            </p>
+        </div>
+        <div class="foot-link">
+            <ul class="foot-li">
+                <h3>
+                    <strrong>Useful Links</strrong>
+                </h3>
+                <li><a href="#">About Us</a></li>
+                <li> <a href="#">Our Authors</a></li>
+                <li> <a href="#">Submit Your Article</a></li>
+                <li><a href="#">Find out church</a></li>
+                <li><a href="#">Add your church</a></li>
+                <li><a href="#">Contact us </a></li>
+            </ul>
+        </div>
+        <div class="foot-search">
+            <h3><strong>Search</strong></h3>
+            <input type="text">
+            <button type="text" class="foot-text">Search</button>
+        </div>
+    </div>
+</body>
+<?php wp_footer(); ?>
+</html>
+
---
+++ index.php
@@ -0,0 +1,115 @@
+<div class="content-item">
+        <h1>The Man Who Climbed The tree</h1>
+    </div>
+    <div class="content-paragraph">
+        <p>Zacchaeus encounters Jesus and his life turns around. He gives up <br>his belongings to the poor and repays
+            to those he swindled. How<br> did this all happen?
+        </p>
+    </div>
+    <div class="content-image">
+        <img src="church.webp" width="900" height="1000px">
+    </div>
+    <div class="content-para">
+        <p>I’m writing this just to get the record straight about the recent events that had <br>unfolded in our town.
+            In short, yes it’s true that I gave away most of my belongings<br> to the poor and repaid those from whom I
+            had swindled. So yes, I’m <br>acknowledging that I have abused my power of the position that I hold. And no,
+            I<br> was not blackmailed into giving back the money. I did what I did of my own <br>accord. Since I
+            understand your skepticism, I wanted to clarify my side of the story.</p>
+        <p>Couple of months ago, I met Matthew, the tax collector, in Jerusalem. We have <br>
+            known each other for quite a long time. Went to the same school. We actually <br>
+            spent most of our childhood together. Those who know me would certify that I’m <br>
+            an introvert who does not have a lot of friends. But Matthew was always an <br>exception.
+
+            He was closest to the brother I never had. He understood me better <br>than most people.
+            As tax collectors, we are always misunderstood and looked<br> down on as traitors because
+            we extort money from our own people to establish <br>Roman rule. We obviously shared the
+            same pain of being ostracized from our own. We would spend hours together criticizing our own<br>
+            people, talking about politics and debating on the scriptures.</p>
+        <p>The ‘Matthew’ that I met last in Jerusalem was indeed a changed man. <br>
+            I could see that he was far more composed than I have ever seen him to be. <br>
+            \In our whole course of conversation, he never brought up his share of hurt and pain.<br>
+            It seemed as if he was beyond that. As if he had been healed from all the hurt and it <br>
+            didn’t bother him anymore. There was so much compassion in his eyes. He mentioned that <br>
+            he had quit his job. His life had been turned around since the time he decided <br>
+            to follow Jesus, the Rabbi. I could not believe what I was seeing.</p>
+        <p>How was this even possible? Was Jesus really the man whom Matthew<br>
+            proclaimed? Seeing Matthew, I immediately recognized the depravity<br>
+            of my soul, how poor and sick I was. Compared to me, Matthew was always <br>
+            the better person. He never extorted money or took advantage of people like <br>
+            I did. On my way back, I kept wondering if there was any redemption for a person like me.
+        </p>
+        <p>It was around noon yesterday, when I heard the news that Jesus was walking through my town.<br>
+            People in the town had varied opinions of him. Some thought he was a prophet. Others thought<br>
+            he was going to be the king of Israel who would eventually redeem us from the Romans. <br>
+            Some even thought he was a false prophet claiming to be the Messiah.<br>
+            I wanted to see who this Jesus was for myself. When I went to the town <br>
+            square to see him, I saw a multitude of crowd around him. Well, it <br>
+            was impossible to even have a glimpse of him since I am short.<br>
+            I was so desperate to see him that I decided to run ahead. <br>
+            I forgot everything and climbed a sycamore tree just to have <br>
+            better look at him. It was also in the hope of remaining unnoticed.</p>
+        <p>It was around noon yesterday, when I heard the news that Jesus <br>
+            was walking through my town. People in the town had varied opinions <br>
+            of him. Some thought he was a prophet. Others thought he was going <br>
+            to be the king of Israel who would eventually redeem us from the<br>
+            Romans. Some even thought he was a false prophet claiming to be <br>
+            the Messiah. I wanted to see who this Jesus was for myself.<br>
+            When I went to the town square to see him,<br>
+            I saw a multitude of crowd around him. Well, <br>
+            it was impossible to even have a glimpse of him since I am short. <br>
+            I was so desperate to see him that I decided to run ahead. <br>
+            I forgot everything and climbed a sycamore tree just to <br>
+            have a better look at him. It was also in the hope of remaining unnoticed.
+
+        </p>
+    </div>
+    <hr class="content-line">
+    <div class="content-entry">
+        <h1>ABOUT THE AUTHOR</h1>
+    </div>
+    <div class="content-user">
+        <img src="user-image.png">
+        <div class="user-name">
+            <a href="#">
+                <h2>Prateek Angel</h2>
+            </a>
+            <p>Involved in ministry and is an active musician,coder and blogger</p>
+            <div class="content-icon">
+                <i class="fa-solid fa-earth-americas"></i>
+                <p>Website</p>
+            </div>
+        </div>
+    </div>
+    <?php get_header(); ?>
+    <?php get_footer(); ?>
+    <div class="content-btn">
+        <h2>Leave a Reply</h2>
+    </div>
+    <div class="content-media">
+        <p>Connect with:</p>
+        <a href="#" class="spin"><i class="fa-brands fa-facebook fa-spin fa-spin-reverse"></i></a>
+        <a href="#" class="spin"><i class="fa-brands fa-google fa-spin fa-spin-reverse"></i></a>
+        <p>Your email address will not be published. Required fields are marked*</p>
+        <p class="content-comment">COMMENT*</p>
+        <textarea id="content-area"></textarea>
+        <br>
+        <label for="name" class="content-name">NAME*</label><br>
+        <input type="text" class="content-text"><br>
+        <label for="email" class="content-name">EMAIL*</label><br>
+        <input type="text" class="content-text"><br>
+        <label for="website" class="content-name">WEBSITE</label><br>
+        <input type="text" class="content-text"><br>
+        <button type="text" class="content-button">POST COMMENT</button>
+        <hr class="content-last">
+        <p class="last-para">Previous</p>
+        <a href="#">
+            <h2 class="content-h2">Judge not, that you be not judged!</h2>
+        </a>
+        <hr class="content-middle">
+        <p class="last-para">Next</p>
+        <a href="#">
+            <h2 class="content-h2">What Happened Brother?</h2>
+        </a>
+        <hr class="content-middle">
+    </div>
+    
